ef88354f722a89fe0c6755ecaa7a097cd6b10088
Noal Smith <noal.smith@kyrus-tech.com>
2025-02-01 10:36:27 -0700

    adding auto routing

--- eslint.config.ts
+++ eslint.config.ts
@@ -21,4 +21,10 @@ export default defineConfigWithVueTs(
   pluginVue.configs['flat/essential'],
   vueTsConfigs.recommended,
   skipFormatting,
+
+  {
+    rules: {
+      'vue/multi-word-component-names': 0,
+    },
+  },
 )
